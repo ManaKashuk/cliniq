594d2c98d1a02b2f38a0a659416839de02ee11f9
Mana Kashuk <140280356+ManaKashuk@users.noreply.github.com>
2025-12-17 16:24:34 -0600

    Update app.py

--- app.py
+++ app.py
@@ -223,7 +223,7 @@ def main():
     st.markdown(
         f"""
         <div class="hero-wrap">
-            <img src="data:image/png;base64,{(logo_b64 or icon_b64)}" style="height:64px;border-radius:8px;"/>
+            <img src="data:image/png;base64,{(logo_b64 or icon_b64)}" style="height:164px;border-radius:8px;"/>
             <div class="hero">
                 <h1>💡 Smart Assistant for Clinical Trial SOP Navigation</h1>
                 <p>I am trained on institutional Standard Operating Procedures (SOPs) and compliance frameworks, helping research teams navigate essential documentation, regulatory requirements, and."
